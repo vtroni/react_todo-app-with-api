c495f5f89ef71a738d4a7f87983a8e1b9827ba08
Virineya Marchuk <marchukvirineya@gmail.com>
2025-01-17 22:58:36 +0200

    solution

---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+interface TodoHeaderProps {
+  newTitle: string;
+  setNewTitle: React.Dispatch<React.SetStateAction<string>>;
+  addTodo: (e: React.FormEvent<HTMLFormElement>) => void;
+  isInputDisabled: boolean;
+  inputRef: React.RefObject<HTMLInputElement>;
+  handleToggleAll: () => void;
+}
+
+const TodoHeader: React.FC<TodoHeaderProps> = ({
+  newTitle,
+  setNewTitle,
+  addTodo,
+  isInputDisabled,
+  inputRef,
+  handleToggleAll,
+}) => {
+  return (
+    <header className="todoapp__header">
+      <button
+        type="button"
+        className={`todoapp__toggle-all`}
+        data-cy="ToggleAllButton"
+        onClick={handleToggleAll}
+      />
+
+      <form onSubmit={addTodo}>
+        <input
+          data-cy="NewTodoField"
+          type="text"
+          className="todoapp__new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          value={newTitle}
+          onChange={e => setNewTitle(e.target.value)}
+          ref={inputRef}
+          disabled={isInputDisabled}
+        />
+      </form>
+    </header>
+  );
+};
+
+export default TodoHeader;
